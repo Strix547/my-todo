858054891c6e50403a52a21380209bd922809f4a
Strix547 <strix547@mail.ru>
2019-02-22 02:51:24 +0300

    Updates

---
+++ precache-manifest.3c45fcb1a93950024fe6bc59b7b00b33.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "58bf8e0bd1dd213bf745e9d748caf175",
+    "url": "/my-todo/static/media/Roboto.58bf8e0b.woff"
+  },
+  {
+    "revision": "e9711dce207c253c72b5",
+    "url": "/my-todo/static/css/main.d23c4ba4.chunk.css"
+  },
+  {
+    "revision": "75f417504cd7d8c70708",
+    "url": "/my-todo/static/js/1.75f41750.chunk.js"
+  },
+  {
+    "revision": "400411c29f51e0fcc38b",
+    "url": "/my-todo/static/js/runtime~main.400411c2.js"
+  },
+  {
+    "revision": "69eee0b43d46be3ad91ef75a76ea3959",
+    "url": "/my-todo/static/media/Roboto.69eee0b4.woff2"
+  },
+  {
+    "revision": "38e1ed9a9ee49d755d20226b0d66588c",
+    "url": "/my-todo/static/media/Robotolight.38e1ed9a.woff2"
+  },
+  {
+    "revision": "4a34361b26e9896ea1b5065708f8daea",
+    "url": "/my-todo/static/media/Robotomedium.4a34361b.woff2"
+  },
+  {
+    "revision": "15a913e1228cca9b1247b373bdf27e55",
+    "url": "/my-todo/static/media/Robotobold.15a913e1.woff2"
+  },
+  {
+    "revision": "e9711dce207c253c72b5",
+    "url": "/my-todo/static/js/main.e9711dce.chunk.js"
+  },
+  {
+    "revision": "611ac98371c2d77c377041e1a0fd87dc",
+    "url": "/my-todo/static/media/Robotolight.611ac983.woff"
+  },
+  {
+    "revision": "6422eac71f54ee5cd007dcf46fc0d22d",
+    "url": "/my-todo/static/media/Robotomedium.6422eac7.woff"
+  },
+  {
+    "revision": "7bf74824d7a8b3c08fe5956f93e9ce2a",
+    "url": "/my-todo/static/media/Robotobold.7bf74824.woff"
+  },
+  {
+    "revision": "c613ca81d36649b260700f3335d8f579",
+    "url": "/my-todo/static/media/Roboto.c613ca81.ttf"
+  },
+  {
+    "revision": "63d6f579b0db1fc75cc977a0539ce45f",
+    "url": "/my-todo/static/media/Robotolight.63d6f579.ttf"
+  },
+  {
+    "revision": "38aa81d461c299fa9f0cb27726401996",
+    "url": "/my-todo/static/media/Robotomedium.38aa81d4.ttf"
+  },
+  {
+    "revision": "1fc6a83d1e366dfe7babb690d33ba6fb",
+    "url": "/my-todo/static/media/Robotobold.1fc6a83d.ttf"
+  },
+  {
+    "revision": "ba6b191b95d512acc4489f6dc853d17a",
+    "url": "/my-todo/index.html"
+  }
+];
